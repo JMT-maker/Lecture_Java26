390e3ec9b50ce7d94fea4ed6beb5c45aeed65d20
JMT-maker <jmtang0525@gmail.com>
2026-08-21 03:02:46 +0900

    Rename package from day01 to Day01

---
+++ Day01/While01.java
@@ -0,0 +1,23 @@
+package Day01;
+
+import java.util.Scanner;
+
+public class While01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = 0;
+		int sum = 0;
+		while(true) {
+			System.out.print("숫자를 입력하세요 ");
+			num = sc.nextInt();
+			sum += num;
+			if(num==0) break;
+		}
+		
+		System.out.println("지금까지 입력한 숫자의 합은 " + sum + "입니다.");
+
+	}
+
+}
