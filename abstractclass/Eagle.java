0d0652142300768a2e12e0b4f1ca8c01658feb74
JMT-maker <jmtang0525@gmail.com>
2026-08-26 03:30:16 +0900

    Remove super constructor call in Eagle class
    Removed call to super constructor in Eagle class.

---
+++ abstractclass/Eagle.java
@@ -0,0 +1,15 @@
+package abstrctclass;
+
+public class Eagle extends Animal{
+
+	public Eagle(String meal) {
+		this.meal = meal;
+	}
+
+	@Override
+	public void move() {
+		System.out.println("날아다닌다.");
+		
+	}
+
+}
